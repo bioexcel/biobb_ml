4d35f6afc112c691f55e753050cec09574f1fdb8
gbayarri <genis.bayarri@irbbarcelona.org>
2021-08-04 15:36:56 +0200

    Trying to fix read the docs TF problems

--- biobb_ml/docs/source/conf.py
+++ biobb_ml/docs/source/conf.py
@@ -18,7 +18,7 @@ from pathlib import Path
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, str(Path('../../').resolve()))
+sys.path.insert(0, str(Path('../../../').resolve()))
 print(sys.executable)
 print(sys.path)
 
