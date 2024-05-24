24b722bee0bb415660a51a44d537b346ef9b3dbc
gbayarri <genis.bayarri@irbbarcelona.org>
2024-05-24 13:28:58 +0200

    2024.1

--- biobb_ml/__init__.py
+++ biobb_ml/__init__.py
@@ -1,3 +1,3 @@
 name = "biobb_ml"
 __all__ = ["classification", "clustering", "dimensionality_reduction", "regression", "resampling", "neural_networks", "utils"]
-__version__ = "4.1.0"
+__version__ = "4.2.0"
